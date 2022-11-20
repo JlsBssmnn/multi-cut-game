86cb7c8b58918094afa88740bd7433a0347a4216
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2022-11-20 16:17:05 +0100

    choose 1 or -1 as costs for edges

--- graphs/fullyConnected.ts
+++ graphs/fullyConnected.ts
@@ -3,13 +3,17 @@ import { LogicalGraph } from "../types/graph";
 export default function fullyConnected(numOfNodes: number): LogicalGraph {
   const nodes = Array.from(Array(numOfNodes).keys()).map((i) => ({
     id: i,
-    group: 0,
+    group: i,
   }));
   const edges = [];
 
   for (let i = 0; i < numOfNodes - 1; i++) {
     for (let j = i + 1; j < numOfNodes; j++) {
-      edges.push({ source: i, target: j, value: 1 });
+      edges.push({
+        source: i,
+        target: j,
+        value: 1 - 2 * Number(Math.random() < 0.5),
+      });
     }
   }
 

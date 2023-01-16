e91be58a1d842bc79987ce8683e1d98e9e8f3339
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2023-01-16 10:54:55 +0100

    replace single and normal quotes with html entities

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,11 +15,11 @@ export default function StartPage() {
 
         <h2 className={styles.heading}>What is this game about?</h2>
         <p className={styles.description}>
-          This game let's you interactively solve the multicut problem: You are
-          given a graph with nodes and edges. Each edge has a cost of either -1
-          (red edge) or 1 (green edge). Nodes connected by green edges want to
-          be in the same cluster whereas nodes connected with red edges want to
-          be in different clusters.
+          This game let&apos;s you interactively solve the multicut problem: You
+          are given a graph with nodes and edges. Each edge has a cost of either
+          -1 (red edge) or 1 (green edge). Nodes connected by green edges want
+          to be in the same cluster whereas nodes connected with red edges want
+          to be in different clusters.
           <br />
           There are different levels in ascending difficulty. If you find the
           optimal solution, you will be notified. There is also the option to
@@ -34,7 +34,7 @@ export default function StartPage() {
         <br />
         <div className={styles.bulletPointList}>
           <div className={styles.listItem}>
-            Move a node out of it's cluster
+            Move a node out of it&apos;s cluster
             <div className={styles.animation}>
               <Image src={moveOutAnimation} alt="Move out animation" />
             </div>
@@ -60,7 +60,7 @@ export default function StartPage() {
           </div>
         </div>
 
-        <h2 className={styles.heading}>What's the goal of the game?</h2>
+        <h2 className={styles.heading}>What&apos;s the goal of the game?</h2>
         <p className={styles.description}>
           The goal of the game is to find the optimal solution or one of the
           optimal solutions if there are multiple. The optimal solution is the
@@ -69,7 +69,7 @@ export default function StartPage() {
           your screen size).
         </p>
         <br />
-        <Image src={costLocation} />
+        <Image src={costLocation} alt="current_cost_heighlighted" />
 
         <h2 className={styles.heading}>
           What are the buttons on the picture above doing?
@@ -95,10 +95,10 @@ export default function StartPage() {
             <br />
             This will give you a hint by showing you one edge in the graph that
             is not correct w.r.t. the optimal solution. The incorrect edge will
-            blink for 3 seconds. If you can't see the edge despite not having
-            reached the optimal solution make sure you can see all edges, so no
-            edge is completely overlapping another one. If an edge between two
-            nodes in the same cluster blinks it means that these two nodes
+            blink for 3 seconds. If you can&apos;t see the edge despite not
+            having reached the optimal solution make sure you can see all edges,
+            so no edge is completely overlapping another one. If an edge between
+            two nodes in the same cluster blinks it means that these two nodes
             should actually be in two different clusters. If an edge between two
             clusters blinks it means that some nodes of either cluster should
             actually be in the same cluster. Note that just joining or
@@ -113,33 +113,34 @@ export default function StartPage() {
             This button will open a dialog which shows you how the optimal
             solution looks like. It can help you to get a feeling of how many
             clusters there should be and how large they are going to be. Usually
-            it's not trivial to find the optimal solution just from seeing this
-            one.
+            it&apos;s not trivial to find the optimal solution just from seeing
+            this one.
           </li>
         </ul>
 
         <h2 className={styles.heading}>How to create more levels?</h2>
         <p className={styles.description}>
           You can use the Level Generator to create your own levels! Just
-          specify the parameters in the box on the left and click on "show
-          graph".
+          specify the parameters in the box on the left and click on &quot;show
+          graph&quot;.
           <br />
           You can try to solve your level but the optimal solution will not be
           computed automatically since this can take some time. But you can
-          manually start the calculation by clicking the "compute solution"
-          button. If the solution has been computed you also have access to the
-          three buttons described in the previous section.
+          manually start the calculation by clicking the &quot;compute
+          solution&quot; button. If the solution has been computed you also have
+          access to the three buttons described in the previous section.
           <br />
-          If you want to start over again you can use the "reset graph" button
-          which will reset you multicut to a singleton clustering.
+          If you want to start over again you can use the &quot;reset
+          graph&quot; button which will reset you multicut to a singleton
+          clustering.
           <br />
           If you want to save your level for playing it later or sharing it you
-          can press the "download level" button which will download the graph
-          and, if computed, the optimal solution. The file can be used by going
-          to the Level Import page then clicking on the icon on the bottom left
-          and then selecting the downloaded file. If the file doesn't contain an
-          optimal solution the optimal solution will be computed when the file
-          is loaded.
+          can press the &quot;download level&quot; button which will download
+          the graph and, if computed, the optimal solution. The file can be used
+          by going to the Level Import page then clicking on the icon on the
+          bottom left and then selecting the downloaded file. If the file
+          doesn&apos;t contain an optimal solution the optimal solution will be
+          computed when the file is loaded.
         </p>
 
         <h2 className={styles.heading}>Are you ready for the first level?</h2>
